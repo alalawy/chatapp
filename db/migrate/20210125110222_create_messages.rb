9aa287b13c8a382a3630743781bc912f3d540c54
alalawy <alphacsoft@gmail.com>
2021-01-25 18:10:37 +0700

    add id sender to messages model"

--- db/migrate/20210125110222_create_messages.rb
+++ db/migrate/20210125110222_create_messages.rb
@@ -2,6 +2,7 @@ class CreateMessages < ActiveRecord::Migration[6.1]
   def change
     create_table :messages do |t|
       t.string :idConversation
+      t.string :idSender
       t.string :message
       t.string :messageType
       t.integer :status
